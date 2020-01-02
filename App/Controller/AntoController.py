25954183ad3927397e99184c27571141a63682b4
xiangsiren <981620741@qq.com>
2020-01-02 20:26:32 +0800

    add app.py

--- App/Controller/AntoController.py
+++ App/Controller/AntoController.py
@@ -3,9 +3,10 @@
 
 import sys
 import os
-base_path = os.getcwd()
-sys.path.append(base_path)
-import App.Common.mysql
+import app
+# base_path = os.getcwd()
+# sys.path.append(base_path)
+from Base.base_request import request
 from App.Common.mysql import cli
 from App.Model.Models import *
 from datetime import datetime
@@ -14,6 +15,6 @@ DBsession = cli().get_session()
 case1 = DBsession.query(Case).filter(Case.id == 6).one()
 # DBsession.commit()
 # DBsession.close()
+res = request.run_main(case1.method,case1.path,case1.request_data)
 
-print(type(case1))
-print(case1.path)
+print(res)
