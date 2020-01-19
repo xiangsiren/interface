20867a63d7987c01f8ebc505599bb1aa2e7a0570
xiangsiren <981620741@qq.com>
2020-01-19 23:57:41 +0800

    添加appium测试

--- App/Controller/AntoController.py
+++ App/Controller/AntoController.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from appium import webdriver
+import time
 import sys
 import os
 import app
 # base_path = os.getcwd()
 # sys.path.append(base_path)
+
+
 from Base.base_request import request
 from App.Common.mysql import cli
 from App.Model.Models import *
@@ -32,3 +35,31 @@ print(res)
 # finally:
 #     # DBsession.commit()
 #     DBsession.close()
+
+
+# desired_caps = {}
+# desired_caps['platformName'] = 'Android'
+# desired_caps['noReset'] = 'true'
+# desired_caps['deviceName'] = 'Android Emulator'
+# desired_caps['appWaitActivity'] = 'com.bckj.banmacang.activity.SplashActivity'
+# desired_caps['app'] ='/Users/ren/Documents/new_app_versionName_3.8.1_versionCode_38_packageTest_debug.apk'
+#
+# driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+# time.sleep(5)
+# driver.find_element_by_id('com.bckj.banmacang:id/goto_settings').click()
+# driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+# driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+# driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+# print("3333333333")
+# sizes = driver.get_window_size()
+# x1 = sizes['width']/10 * 9
+# y1 = sizes['height']
+# x = sizes['width']/10
+# driver.swipe(x1,y1,x,y1)
+# driver.swipe(x1,y1,x,y1)
+# driver.swipe(x1,y1,x,y1)
+# print(x1,y1,x)
+# driver.find_element_by_id('com.bckj.banmacang:id/et_login_password_mobile').send_keys("libai123")
+# driver.find_element_by_id('com.bckj.banmacang:id/et_login_password').send_keys("123456")
+# driver.find_element_by_id('com.bckj.banmacang:id/tv_login').click()
+
