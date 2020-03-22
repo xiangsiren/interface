ad5a53887fe103e9d85f851239f505de083b1d27
xiangsiren <981620741@qq.com>
2020-03-22 17:40:29 +0800

    添加appium测试

--- seleniumPython/ThreeNode/requests_open_browser.py
+++ seleniumPython/ThreeNode/requests_open_browser.py
@@ -1,17 +1,22 @@
 #coding=utf-8
 import requests
 import json
+
+
 class requests_webdriver:
+
     def __init__(self):
         self.driver = self.chrome_driver()
     def chrome_driver(self):
         url = 'http://127.0.0.1:4444/wd/hub/session/'
+        # url = 'http://127.0.0.1:4444/wd/hub'
         data = json.dumps({
             'Capabilities':{
                 'browserName':'chrome'
             }
         })
-        res = requests.post(url,data).json()
+        res = requests.post(url, data).json()
+        print(res)
         session = res['sessionId']
         driver = url+session
         return driver
@@ -32,7 +37,8 @@ class requests_webdriver:
         res = requests.post(base_url,data).json()['value']['ELEMENT']
         return res
 
+
 if __name__ == '__main__':
     request_driver = requests_webdriver()
-    request_driver.get_url('https://www.imooc.com/user/newlogin/from_url/')
-    request_driver.find_element_by_id('email')
+    # request_driver.get_url('https://saas-test.11xiuge.com/')
+    # request_driver.find_element_by_id('email')
